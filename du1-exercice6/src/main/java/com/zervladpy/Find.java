9601945348bc22b378fc9c6e78bf48a26f7124e6
zervladpy <zernovvladislav7@gmail.com>
2023-12-07 14:19:28 +0000

    -a `du1-exercice6`

---
+++ du1-exercice6/src/main/java/com/zervladpy/Find.java
@@ -0,0 +1,38 @@
+package com.zervladpy;
+
+import java.io.File;
+
+/**
+ * Create a program to exect the command find in Ububtu to count the number of
+ * lines that one text appears in one file. The number obtained must be write to
+ * another file. The text, the name of the file where to search the text and the
+ * name of the file where to put the result must be arguments in the command
+ * line. Use the ProcessBuilder methods redirectInput and redirectOutput.
+ */
+public class Find {
+    public static void main(String[] args) {
+
+        if (args.length != 3) {
+            System.out.println("Usage: java -jar du1-exercice6.jar <text> <file> <result_file>");
+            System.exit(1);
+        }
+
+        String text = args[0];
+        String file = args[1];
+        String result = args[2];
+
+        String[] command = { "find", file, "-type", "f", "-exec", "grep", "-c", text, "{}", ";" };
+
+        // count how many times the text appears in the file
+        ProcessBuilder pb = new ProcessBuilder(command);
+        pb.redirectOutput(new File(result));
+
+        try {
+            Process p = pb.start();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
