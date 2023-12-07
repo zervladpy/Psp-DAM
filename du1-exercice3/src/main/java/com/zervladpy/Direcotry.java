6c7e55095a6346d4dfed85c6307d7766c6ae9b29
zervladpy <zernovvladislav7@gmail.com>
2023-12-07 13:39:25 +0000

    -u updated Java ClassName

--- du1-exercice3/src/main/java/com/zervladpy/Direcotry.java
+++ du1-exercice3/src/main/java/com/zervladpy/Direcotry.java
@@ -3,7 +3,7 @@ package com.zervladpy;
 import java.io.File;
 import java.util.Scanner;
 
-public class App {
+public class Direcotry {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
