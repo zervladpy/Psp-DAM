95ab11b676b4e04a0430eb184a5fd3c0bc38cfce
zervladpy <zernovvladislav7@gmail.com>
2023-12-08 14:30:16 +0000

    -a `du2-exercice2`

---
+++ du2-exercice2/src/main/java/com/zervladpy/HiddenNumber.java
@@ -0,0 +1,27 @@
+package main.java.com.zervladpy;
+
+import java.util.Random;
+
+public class HiddenNumber {
+
+    private int hiddenNumber;
+    private int gamseStatus = 0;
+
+    public HiddenNumber(int minValue, int maxValue) {
+        this.hiddenNumber = new Random().nextInt(minValue, maxValue);
+    }
+
+    public int numberGuess(int num) {
+        if (gamseStatus == 0) {
+            if (num == hiddenNumber) {
+                gamseStatus = 1;
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+}
