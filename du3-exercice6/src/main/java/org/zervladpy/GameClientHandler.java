918a6b1c4af2fd75cf58809c5d2475068bba3da5
zervladpy <zernovvladislav7@gmail.com>
2024-01-23 14:31:14 +0100

    -u `du3-exercice6`

--- du3-exercice6/src/main/java/org/zervladpy/GameClientHandler.java
+++ du3-exercice6/src/main/java/org/zervladpy/GameClientHandler.java
@@ -4,6 +4,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class GameClientHandler implements Runnable {
@@ -17,11 +19,18 @@ public class GameClientHandler implements Runnable {
     private DataOutputStream out;
     private DataInputStream in;
 
+    private List<String> commands;
+
     GameClientHandler(Socket client) {
         this.client = client;
         gameOnGoing = false;
         connectionOpen = true;
 
+        commands = new ArrayList<>();
+        commands.add("NEW");
+        commands.add("QUIT");
+        commands.add("HELP");
+        commands.add("NUM");
     }
 
     @Override
@@ -37,38 +46,67 @@ public class GameClientHandler implements Runnable {
 
                 String input = in.readUTF();
 
-                if (input.startsWith("NEW")) {
-                    totalGuesses = Integer.parseInt(input.substring(0, 4));
-                    hiddenNumber = new Random().nextInt(10);
-                    out.writeUTF("20 PLAY " + totalGuesses);
+                boolean isValid = false;
+
+                for (String cmd : commands) {
+                    if (input.contains(cmd)) {
+                        isValid = true;
+                        break;
+                    }
+                }
+
+                if (!isValid) {
+                    out.writeUTF("90 UNKNOWN");
+                }
+
+                if (input.startsWith("NEW") && !gameOnGoing) {
+
+                    int n = getNumber(input);
+
+                    if (n == -1) {
+                        out.writeUTF("90 UNKNOWN");
+                    } else if (n > -1) {
+                        totalGuesses = getNumber(input);
+                        hiddenNumber = new Random().nextInt(10);
+                        out.writeUTF("20 PLAY " + totalGuesses);
+                    }
+
                 }
 
                 if (input.startsWith("NUM")) {
-                    int n = Integer.parseInt(input.substring(0, 4));
+                    int n = getNumber(input);
 
-                    if (n < hiddenNumber) {
-                        out.writeUTF("25 LOW");
+                    if (n == -1) {
+                        out.writeUTF("90 UNKNOWN");
+                    } else if (n > -1) {
+                        if (n < hiddenNumber) {
+                            out.writeUTF("25 LOW " + (currentGuesses - totalGuesses));
 
+                        }
+
+                        if (n > hiddenNumber) {
+                            out.writeUTF("35 HIGH " + (currentGuesses - totalGuesses));
+                        }
+
+                        if (n == hiddenNumber) {
+                            out.writeUTF("50 WIN");
+                        }
                     }
 
-                    if (n > hiddenNumber) {
-                        out.writeUTF("35 HIGH");
-                    }
 
-                    if (n == hiddenNumber) {
-
-                    }
                 }
 
                 if (input.startsWith("HELP")) {
-
+                    out.writeUTF("NEW <attempts> - new game with user given attempts\nNUM <guess> - try guess\nHELP - help menu\nQUIT - end game");
                 }
 
                 if (input.equals("QUIT")) {
+                    out.writeUTF("11 BYE");
                     connectionOpen = false;
                 }
 
             }
+            client.close();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -76,8 +114,14 @@ public class GameClientHandler implements Runnable {
 
     }
 
-    public void read() {
+    private int getNumber(String str) throws IOException {
+        try {
+            return Integer.parseInt(str.substring(4));
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            out.writeUTF("80 ERR");
 
+            return -1;
+        }
     }
 
 }
